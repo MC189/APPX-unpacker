87beabe3871cd175f96ba7e73180d38144107f8f
MC <62210133+MC189@users.noreply.github.com>
2023-03-02 19:14:21 +0700

    Enhance
    - Appx name as Direcotry Name

--- Appx-unpacker-for-python-3.py
+++ Appx-unpacker-for-python-3.py
@@ -2,8 +2,7 @@ import zipfile
 import os
 print("make sure this is running in the same directory as your appx file")
 appxfile=input("please enter the file name of the appx you want to unpack: ")
-folder=appxfile.replace(".appx","")
-folder=folder.replace("bundle", "")
+folder=appxfile.removesuffix('.Appx')
 if not os.path.exists(folder):
     os.makedirs(folder)
 zip_ref = zipfile.ZipFile(appxfile, 'r')
